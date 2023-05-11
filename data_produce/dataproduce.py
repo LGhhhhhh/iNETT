07d71ee7beb29eaff6024e84692074d2264047de
LGhhhhhh <90737860+LGhhhhhh@users.noreply.github.com>
2023-05-11 09:41:39 +0800

    1.dataproduce

--- data_produce/dataproduce.py
+++ data_produce/dataproduce.py
@@ -1,9 +1,10 @@
+
 import numpy as np
 import odl
 from art_fun import art_fun
 from wgn_fun import wgn_fun
 import matplotlib.pyplot as plt
-w = np.loadtxt('E:/GH/AO/lungweight.txt')
+w = np.loadtxt('E:/GH/AO/lungweight.txt')#投影矩阵
 
 k = 5
 r = 1
@@ -27,20 +28,20 @@ def random_phantom(spc):
     return odl.phantom.ellipsoid_phantom(spc, ellipses)
 
 
-phantom = random_phantom(space)
-f = np.reshape(phantom, [size, size])
-f = np.array(f).T
-# if np.max(f) > 0:
-#     f = 1 / np.max(f) * f #归一化
-plt.imshow(f)
+# phantom = random_phantom(space)
+# f = np.reshape(phantom, [size, size])
+# f = np.array(f).T
+# # if np.max(f) > 0:
+# #     f = 1 / np.max(f) * f #归一化
+# plt.imshow(f)
 
 train_true_image = np.empty([2000, 256, 256, 1])
 train_set_data = np.empty([2000, 15360, 1])  # 15360 =256*60
 train_set = np.empty([2000, 256, 256, 1])
 label_set = np.empty([2000, 256, 256, 1])
 # plt.imshow((label_set[0, ..., 0]), cmap='gray')
-"""生成1000个无伪影训练集"""
-for i in range(1000, 2000):
+"""生成1000个有伪影训练集"""
+for i in range(0, 1000):
     noise_num = 0.1 * np.random.random()
     phantom = random_phantom(space)
     f = np.reshape(phantom, [size, size])
@@ -50,20 +51,26 @@ for i in range(1000, 2000):
     train_true_image[i, ..., 0] = f #训练集真实图像
     p = np.dot(w, np.reshape(f, [256 * 256, 1]))
     b_noisy = wgn_fun(p, noise_num)
-    train_set_data[i, ..., 0] = b_noisy #训练集投影数据（加噪声）
+    train_set_data[i] = b_noisy #训练集投影数据（加噪声）
+    rec, F = art_fun(w, b_noisy, k, r)
+    rec = np.reshape(rec, [256, 256])
+    train_set[i, ..., 0] = rec
+    label_set[i, ..., 0] = np.abs(train_true_image[i, ..., 0] - train_set[i, ..., 0])
     print(i)
 
 
     """1000个无伪影的训练集"""
 for i in range(1000, 2000):
     train_set[i, ..., 0] = train_true_image[i, ..., 0]
+    f = np.reshape(phantom, [size, size])
+    f = np.array(f).T
+    if np.max(f) > 0:
+        f = 1 / np.max(f) * f  # 归一化
+    train_true_image[i, ..., 0] = f  # 训练集真实图像
+    train_set[i, ..., 0] = f
+    p = np.dot(w, np.reshape(f, [256 * 256, 1]))
+    train_set_data[i] = p
     print(i)
-    """1000个伪影的训练集"""
-for i in range(1000):
-    train_set[i, ..., 0] = train_set0[i, ..., 0]
-    train_set_data[i] = train_set_data0[i]
-    train_true_image = train_true_image0[i, ..., 0]
-    label_set[i, ..., 0] = label_set0[i, ..., 0]
 
 np.save(path2 + 'train_true_image.npy', train_true_image)
 np.save(path2 + 'train_set_data.npy', train_set_data)
@@ -78,7 +85,7 @@ test_set_data = np.empty([400, 15360, 1])  # 15360 =256*60
 test_set = np.empty([400, 256, 256, 1])
 test_label_set = np.empty([400, 256, 256, 1])
 """生成200个无伪影训练集"""
-for i in range(200, 400):
+for i in range(0, 200):
     noise_num = 0.1 * np.random.random()
     phantom = random_phantom(space)
     f = np.reshape(phantom, [size, size])
@@ -88,20 +95,27 @@ for i in range(200, 400):
     test_true_image[i, ..., 0] = f #训练集真实图像
     p = np.dot(w, np.reshape(f, [256 * 256, 1]))
     b_noisy = wgn_fun(p, noise_num)
-    test_set_data[i, ..., 0] = b_noisy #训练集投影数据（加噪声）
+    test_set_data[i] = b_noisy #训练集投影数据（加噪声）
+    rec, F = art_fun(w, b_noisy, k, r)
+    rec = np.reshape(rec, [256, 256])
+    test_set[i, ..., 0] = rec
+    test_label_set[i, ..., 0] = np.abs(test_true_image[i, ..., 0] - test_set[i, ..., 0])
     print(i)
 
 
     """200个无伪影的训练集"""
 for i in range(200, 400):
-    test_set[i, ..., 0] = test_true_image[i, ..., 0]
+    f = np.reshape(phantom, [size, size])
+    f = np.array(f).T
+    if np.max(f) > 0:
+        f = 1 / np.max(f) * f  # 归一化
+    test_true_image[i, ..., 0] = f  # 训练集真实图像
+    test_set[i, ..., 0] = f
+    p = np.dot(w, np.reshape(f, [256 * 256, 1]))
+    test_set_data[i] = p
     print(i)
-    """200个伪影的训练集"""
-for i in range(200):
-    test_set[i, ..., 0] = test_set0[i, ..., 0]
-    test_set_data[i] = test_set_data0[i]
-    test_true_image = test_true_image0[i, ..., 0]
-    test_label_set[i, ..., 0] = test_label_set0[i, ..., 0]
+
+
 np.save(path2 + 'validation_true_image.npy', test_true_image)
 np.save(path2 + 'validation_set_data.npy', test_set_data)
 np.save(path2 + 'validation_set.npy', test_set)
@@ -115,9 +129,8 @@ predict_set_data = np.empty([200, 15360, 1])  # 155360 =256*60
 predict_set = np.empty([200, 256, 256, 1])
 predict_label_set = np.empty([200, 256, 256, 1])
 
-"""生成200个测试集"""
-for i in range(200):
-    print(i)
+"""生成100个测试集"""
+for i in range(100):
     noise_num = 0.05
     phantom = random_phantom(space)
     f = np.reshape(phantom, [size, size])
@@ -127,20 +140,32 @@ for i in range(200):
     predict_true_image[i, ..., 0] = f  # 验证集真实图像
     p = np.dot(w, np.reshape(f, [256 * 256, 1]))
     b_noisy = wgn_fun(p, noise_num)
-    predict_set_data[i, ..., 0] = b_noisy #训练集投影数据（加噪声）
-
-"""200个有伪影的测试集"""
-for i in range(200):
-    p = predict_set_data[i, ..., 0]
-    rec, F = art_fun(w, p, k, r)
+    predict_set_data[i] = b_noisy #训练集投影数据（加噪声）
+    rec, F = art_fun(w, b_noisy, k, r)
     rec = np.reshape(rec, [256, 256])
-    predict_label_set[i, ..., 0] = predict_true_image[i, ..., 0] - rec  # 有伪影的label为伪影 （原始图像减去ART算法后的图像）
     predict_set[i, ..., 0] = rec
+    predict_label_set[i, ..., 0] = np.abs(predict_true_image[i, ..., 0] - predict_set[i, ..., 0])
     print(i)
 
-np.save(path1 + 'predict_true_image.npy', predict_true_image)
-np.save(path1 + 'predict_set_data.npy', predict_set_data)
-np.save(path1 + 'predict_set.npy', predict_set)
-np.save(path1 + 'predict_set_label.npy', predict_label_set)
+"""200个有伪影的测试集"""
+for i in range(100, 200):
+    phantom = random_phantom(space)
+    f = np.reshape(phantom, [size, size])
+    f = np.array(f).T
+    if np.max(f) > 0:
+        f = 1 / np.max(f) * f  # 归一化
+    predict_true_image[i, ..., 0] = f  # 验证集真实图像
+    predict_set[i, ..., 0] = f
+    p = np.dot(w, np.reshape(f, [256 * 256, 1]))
+    predict_set_data[i] = p
+
+np.save(path2 + 'predict_true_image.npy', predict_true_image)
+np.save(path2 + 'predict_set_data.npy', predict_set_data)
+np.save(path2 + 'predict_set.npy', predict_set)
+np.save(path2 + 'predict_set_label.npy', predict_label_set)
+
+
+
+
 
 
